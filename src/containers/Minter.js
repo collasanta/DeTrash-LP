59410d3ff2aaed62572128cfdb16cd8203daa2ed
Victor Collasanta <victor.collasanta@gmail.com>
2022-04-07 07:29:09 -0300

    v1.2

--- src/containers/Minter.js
+++ src/containers/Minter.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import { useState } from 'react';
 import { images } from '../assets';
 
 const styles = {
    bg: `bg-[#ffffff] p-10 flex justify-center`,
-   container: `p-6  flex flex-col sm:flex-row justify-center bg-[#f2f5fa] md:max-w-2xl shadow-lg  `,
-   div1: ` p-2 sm:w-1/2 flex flex-col justify-center text-center space-y-3`,
-   div2: ` p-2 sm:w-1/2 align-middle text-center flex flex-col justify-center space-y-4` ,
-   image: ` px-8 py-0 `,
-   supply: `font-[pix] text-lg bg-[#ffffff] mx-10`,
-   btnconnect: `font-[pix] animate-pulse text-lg bg-[#6e45c7] hover:bg-[#45c76e] text-white font-bold py-6 px-4 mx-2 shadow-md`,
+   container: `px-3 py-3 flex flex-col sm:flex-row justify-center bg-[#f2f5fa] md:max-w-2xl shadow-lg  `,
+   div1: ` p-2 sm:w-1/2 flex flex-col justify-center text-center space-y-2`,
+   div2: ` p-2 sm:w-1/2 align-middle text-center flex flex-col justify-center space-y-2` ,
+   image: ` px-6 py-0 `,
+   supply: `font-[pix] text-lg bg-[#ffffff] mx-6`,
+   btnconnect: `font-[pix] animate-pulse text-lg bg-[#6e45c7] hover:bg-[#45c76e] text-white font-bold p-4  shadow-md`,
    amount: `font-[pix] text-lg`,
-   btnmint: `font-[pix] text-lg bg-[#6e45c7] hover:bg-[#45c76e] text-white font-bold py-6 px-4 mx-2 shadow-md`,
+   btnmint: `font-[pix] text-lg bg-[#6e45c7] hover:bg-[#45c76e] text-white font-bold py-4 px-6 shadow-md`,
    counterbtnp: `bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l`,
    counterbtnn: `bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r`,
    counter: `bg-gray-200 text-gray-800 font-bold py-3 px-4`,
@@ -18,6 +19,9 @@ const styles = {
 }
 
 const Minter = () => {
+   const [mintAmount, setmintAmount] = useState(1);
+   const NFTprice = 0.75 ; //MATIC
+
   return (
     <>
    
@@ -35,26 +39,30 @@ const Minter = () => {
                   <span className={styles.asupply}>30</span>/10.000
                </div>
 
-               <div className={styles.supply}>
-                  <span>PRICE: </span>
-                  <span className={styles.asupply}>1</span> MATIC
-               </div>
-
             </div>
 
             <div className={styles.div2}>
-               
+
+
                <div className={styles.btnconnect}>
                   <button>CONNECT WALLET</button>
                </div>
 
+                              
+
+
                <div className={styles.amount}>
                   <div>Amount:</div>
-                  <button className={styles.counterbtnp}>-</button>
-                  <span className={styles.counter}>1</span>
-                  <button className={styles.counterbtnn}>+</button>
+                  <button className={styles.counterbtnp} onClick={() => {setmintAmount(mintAmount-1)}}>-</button>
+                  <span className={styles.counter}>{mintAmount}</span>
+                  <button className={styles.counterbtnn} onClick={() => {setmintAmount(mintAmount+1)}}>+</button>
                 </div>
 
+               
+                <div className={styles.supply}>
+                  <span>PRICE: </span>
+                  <span className={styles.asupply}>{mintAmount * NFTprice}</span> MATIC
+               </div>
 
                   <button className={styles.btnmint}>MINT</button>
 
